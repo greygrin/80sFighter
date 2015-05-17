7d34614511865092b01e2eec440564c3c7aca69c
greygrin <gearmansion@gmail.com>
2015-05-16 22:01:47 -0400

    The main game scene!

---
+++ BitchinBrawl/Assets/Scripts/QuickDestroy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuickDestroy : MonoBehaviour {
+
+	public float destroyTime = 2f;
+
+	// Use this for initialization
+	void Start () {
+		Destroy(this.gameObject, destroyTime);
+	}
+}
