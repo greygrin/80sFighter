20c5eb31c58a44f6124565a29341b8be9513f60c
Andrew Ho <aho3555@gmail.com>
2015-05-16 21:20:15 -0400

    MusicBehaviorScript
    Son of a bitch!

---
+++ MusicBehavior.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicBehavior : MonoBehaviour 
+{
+	public AudioClip impact;
+	AudioSource audio;
+	GameObject SFX1;
+	
+	void Start() 
+	{
+		SFX1 = GameObject.Find("SFX1");
+		audio = GetComponent<AudioSource>();
+		audio.Play();
+		impact = SFX1.GetComponent<AudioSource>().clip;
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown("x"))
+		{
+			SFX1.GetComponent<AudioSource>().PlayOneShot(impact);
+		}
+	}
+
+}
